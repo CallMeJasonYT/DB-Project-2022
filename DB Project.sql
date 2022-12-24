75202bef69f091efce02618780b74e4f86dbb19f
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2022-12-24 18:41:49 +0200

    Significant Changes Inserts
    -Completed the Manages Table
    -Completed the Guide Table
    -Completed the Languages Table
    -Completed the Trip Table
    -Completed the Event Table
    -Completed the Destination Table
    
    Co-Authored-By: Roumpini21 <85185865+Roumpini21@users.noreply.github.com>

--- DB Project.sql
+++ DB Project.sql
@@ -60,12 +60,12 @@ INSERT INTO worker VALUES
 ('WorkerID03', 'Andreas', 'Oikonomou', 1233.2, 3),
 ('WorkerID04', 'Ektoras', 'Samouil', 820.6, 4),
 ('WorkerID05', 'Konstantinos', 'Kostantinou', 889.1, 5),
-('WorkerID06', 'Iasonas', 'Pavlopoulos', 1776.2, 6),
+('WorkerID06', 'Iasonas', 'Pavlopoulos', 2776.2, 6),
 ('WorkerID07', 'Andreas', 'Andreadis', 1500.32, 7),
 ('WorkerID08', 'Meletios', 'Polidouris', 1222.44, 8),
 ('WorkerID09', 'Loukas', 'Ramakis', 1000.1, 9),
 ('WorkerID10', 'Axilleas', 'Patroklos', 1022.3, 10),
-('WorkerID11', 'Roumpini', 'Aggoura', 1120.7, 1),
+('WorkerID11', 'Roumpini', 'Aggoura', 2620.7, 1),
 ('WorkerID12', 'Maria', 'Louloudi', 433.5, 2),
 ('WorkerID13', 'Prigkipissa', 'Vasilissa', 655.2, 3),
 ('WorkerID14', 'Katerina', 'Papaflessa', 836.44, 4),
@@ -81,7 +81,7 @@ INSERT INTO worker VALUES
 ('WorkerID24', 'Davon', 'Sampson', 977.53, 4),
 ('WorkerID25', 'Declan', 'Kelly', 1024.82, 5),
 ('WorkerID26', 'Donavan', 'Osborn', 845.9, 6),
-('WorkerID27', 'Rashad', 'Noble', 911.6, 7),
+('WorkerID27', 'Rashad', 'Noble', 1230.23, 7),
 ('WorkerID28', 'Eleni', 'Papadaki', 1422.61, 1),
 ('WorkerID29', 'Dionissis', 'Trivizas', 873.21, 2),
 ('WorkerID30', 'Xristos', 'Petropoulos', 965.22, 3),
@@ -150,12 +150,21 @@ CREATE TABLE manages(
 );
 
 INSERT INTO manages VALUES
-('1234567890', 1),
-('6789012345', 2),
-('1098765432', 3),
-('2104319253', 4),
-('9876543210', 5);
-('9012345678', )
+('WorkerID20', 1),
+('WorkerID05', 5),
+('WorkerID23', 3),
+('WorkerID34', 7),
+('WorkerID35', 10),
+('WorkerID11', 1),
+('WorkerID02', 2),
+('WorkerID03', 3),
+('WorkerID24', 4),
+('WorkerID16', 5),
+('WorkerID19', 7),
+('WorkerID08', 8),
+('WorkerID06', 6),
+('WorkerID21', 9),
+('WorkerID22', 10);
 
 CREATE TABLE guide(
     gui_AT CHAR(10) NOT NULL,
@@ -166,13 +175,16 @@ CREATE TABLE guide(
 );
 
 INSERT INTO guide VALUES
-('2345678901', 'Worked as a guide at the Acropolis site for 10 years'),
-('4567890123', 'Worked as a guide at the Santorini island for 5 years'),
-('8901234567', 'Worked as a guide at the Mykonos island for 7 years'),
-('0123456789', 'Worked as a guide at the Acropolis site for 5 years'),
-('7654321098', 'Worked as a guide at the Monemvasia castle for 20 years'),
-('5432109876', 'Worked as a guide at the Acropolis site for 15 years'),
-('3210987654', 'Worked as a guide at the Acropolis site for 1 year');
+('WorkerID01', 'Worked as a guide at the Acropolis site for 10 years'),
+('WorkerID14', 'Worked as a guide at the Santorini island for 5 years'),
+('WorkerID15', 'Worked as a guide at the Mykonos island for 7 years'),
+('WorkerID17', 'Worked as a guide at the Acropolis site for 5 years'),
+('WorkerID18', 'Worked as a guide at the Monemvasia castle for 20 years'),
+('WorkerID27', 'Worked as a guide at the Acropolis site for 15 years'),
+('WorkerID29', 'Worked as a guide at the Acropolis site for 1 year'),
+('WorkerID30', 'Worked as a guide at the Acropolis site for 1 year'),
+('WorkerID31', 'Worked as a guide at the Acropolis site for 1 year'),
+('WorkerID33', 'Worked as a guide at the Acropolis site for 1 year');
 
 CREATE TABLE languages(
     lng_gui_AT CHAR(10) NOT NULL,
@@ -183,23 +195,31 @@ CREATE TABLE languages(
 );
 
 INSERT INTO languages VALUES
-('2345678901','Greek'),
-('4567890123','Greek'),
-('8901234567','Greek'),
-('0123456789','Greek'),
-('7654321098','Greek'),
-('5432109876','Greek'),
-('3210987654','Greek'),
-('2345678901','English'),
-('4567890123','English'),
-('8901234567','English'),
-('0123456789','English'),
-('2345678901','German'),
-('4567890123','German'),
-('2345678901','French'),
-('0123456789','French'),
-('7654321098','Chinese'),
-('5432109876','Chinese');
+('WorkerID01', 'English'),
+('WorkerID01', 'Greek'),
+('WorkerID14', 'English'),
+('WorkerID14', 'Greek'),
+('WorkerID15', 'English'),
+('WorkerID15', 'Greek'),
+('WorkerID17', 'English'),
+('WorkerID17', 'Greek'),
+('WorkerID18', 'English'),
+('WorkerID18', 'Greek'),
+('WorkerID27', 'English'),
+('WorkerID27', 'Spanish'),
+('WorkerID27', 'French'),
+('WorkerID29', 'English'),
+('WorkerID29', 'Greek'),
+('WorkerID30', 'English'),
+('WorkerID30', 'Greek'),
+('WorkerID30', 'Italian'),
+('WorkerID30', 'German'),
+('WorkerID31', 'English'),
+('WorkerID31', 'Greek'),
+('WorkerID33', 'English'),
+('WorkerID33', 'Greek'),
+('WorkerID33', 'Spanish'),
+('WorkerID33', 'French');
 
 CREATE TABLE trip(
     tr_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -220,16 +240,26 @@ CREATE TABLE trip(
 );
 
 INSERT INTO trip VALUES 
-(null, '1992-12-29 15:30:00', '1992-12-30 15:30:00', 20, 25.1, 1, '2345678901', '3456789012'),
-(null, '1992-12-25 15:30:00', '1992-12-26 15:30:00', 2, 23.1, 2, '8901234567', '6543210987'),
-(null, '1992-12-29 11:00:00', '1992-12-30 18:30:00', 22, 23.1, 3, '5432109876', '0987654321'),
-(null, '1993-11-29 15:30:00', '1993-11-30 15:00:00', 21, 21.1, 4, '0123456789', '2109876543'),
-(null, '1992-12-29 15:30:00', '1992-12-30 15:30:00', 22, 20.1, 5, '2345678901', '5678901234'),
-(null, '1992-12-29 15:30:00', '1992-12-30 15:30:00', 25, 23.2, 6, '5432109876', '5678901234'),
-(null, '1996-09-15 15:30:00', '1996-09-17 15:30:00', 26, 28.1, 7, '0123456789', '8765432109'),
-(null, '1994-12-22 15:30:00', '1994-12-28 15:30:00', 27, 29.1, 8, '4567890123', '2109876543'),
-(null, '1992-10-29 10:30:00', '1992-10-30 11:30:00', 27, 24.5, 9, '2345678901', '8765432109'),
-(null, '1992-12-29 15:30:00', '1992-12-30 15:30:00', 27, 23.1, 10, '7654321098', '0987654321');
+(null, '2023-01-29 11:00:00', '2023-02-08 18:00:00', 20, 1023.11, 1, 'WorkerID01', 'WorkerID28'),
+(null, '2023-11-25 10:30:00', '2023-11-30 17:30:00', 5, 361.21, 2, 'WorkerID29', 'WorkerID12'),
+(null, '2023-10-12 11:00:00', '2023-10-16 18:30:00', 15, 451.76, 7, 'WorkerID27', 'WorkerID07'),
+(null, '2023-06-03 12:30:00', '2023-06-15 16:00:00', 6, 842.54, 4, 'WorkerID14', 'WorkerID04'),
+(null, '2023-05-09 11:30:00', '2023-05-20 17:30:00', 9, 822,97, 10, 'WorkerID33', 'WorkerID10'),
+(null, '2023-08-13 11:30:00', '2023-08-23 18:00:00', 50, 677.91, 6, 'WorkerID17', 'WorkerID26'),
+(null, '2023-09-12 12:30:00', '2023-09-17 19:30:00', 30, 532.12, 3, 'WorkerID30', 'WorkerID13'),
+(null, '2023-12-12 10:30:00', '2023-12-20 17:30:00', 15, 742.23, 8, 'WorkerID18', 'WorkerID32'),
+(null, '2023-01-27 12:30:00', '2023-01-30 20:30:00', 17, 352.11, 5, 'WorkerID15', 'WorkerID25'),
+(null, '2023-02-18 11:30:00', '2023-02-28 21:00:00', 20, 874.44, 9, 'WorkerID31', 'WorkerID09'),
+(null, '2023-05-23 11:00:00', '2023-05-30 17:30:00', 10, 858.31, 1, 'WorkerID01', 'WorkerID28'),
+(null, '2023-07-04 12:30:00', '2023-07-17 18:30:00', 4, 923.55, 2, 'WorkerID29', 'WorkerID12'),
+(null, '2023-03-02 12:00:00', '2023-03-08 19:30:00', 22, 690.69, 7, 'WorkerID27', 'WorkerID07'),
+(null, '2023-04-01 11:30:00', '2023-04-06 21:00:00', 14, 570.09, 4, 'WorkerID14', 'WorkerID04'),
+(null, '2023-07-22 12:30:00', '2023-07-28 19:30:00', 5, 700.99, 10, 'WorkerID33', 'WorkerID10'),
+(null, '2023-09-09 11:30:00', '2023-09-15 18:30:00', 4, 790.89, 6, 'WorkerID17', 'WorkerID26'),
+(null, '2023-09-15 10:30:00', '2023-09-19 19:30:00', 7, 350.99, 3, 'WorkerID30', 'WorkerID13'),
+(null, '2023-12-12 11:30:00', '2023-12-17 17:00:00', 10, 500.34, 8, 'WorkerID18', 'WorkerID32'),
+(null, '2023-10-07 12:30:00', '2023-10-10 17:00:00', 30, 357.10, 5, 'WorkerID15', 'WorkerID25'),
+(null, '2023-12-04 10:30:00', '2023-12-15 18:00:00', 45, 1289.99, 9, 'WorkerID31', 'WorkerID09');
 
 CREATE TABLE event(
     ev_tr_id INT(11) NOT NULL,
@@ -242,16 +272,26 @@ CREATE TABLE event(
 );
 
 INSERT INTO event VALUES
-(1, '1992-12-29 15:30:00', '1992-12-30 15:30:00', 'Beautiful trip and lunch at local restaurant'),
-(2, '1992-11-29 15:30:00', '1992-11-30 15:30:00', 'Beautiful trip and dinner at local restaurant'),
-(3, '1992-10-29 15:30:00', '1992-10-30 15:30:00', 'Cruise and activities on the ship'),
-(4, '1992-09-29 15:30:00', '1992-09-30 15:30:00', 'Cruise and activities on the ship'),
-(5, '1992-08-29 15:30:00', '1992-08-30 15:30:00', 'Museum Visit and sightseeing'),
-(6, '1992-07-29 15:30:00', '1992-07-30 15:30:00', 'Museum Visit and sightseeing'),
-(7, '1992-06-29 15:30:00', '1992-06-30 15:30:00', 'Museum Visit and sightseeing'),
-(8, '1992-05-29 15:30:00', '1992-05-30 15:30:00', 'Beautiful trip and dinner at local restaurant'),
-(9, '1992-04-29 15:30:00', '1992-04-30 15:30:00', 'Visiting an Island and dinner at local restaurant'),
-(10, '1992-03-29 15:30:00', '1992-03-30 15:30:00', 'Cruise and activities on the ship');
+(1, '2023-01-31 12:00:00', '2023-01-31 14:00:00', 'Visiting The Louvre Museum.'),
+(2, '2023-11-27 11:30:00', '2023-11-27 14:30:00', 'Learning about the prosperous history of Kalambaka.'),
+(3, '2023-10-13 13:00:00', '2023-10-13 15:30:00', 'Doing a tour of the villages of the island of Crete!'),
+(4, '2023-06-07 13:30:00', '2023-06-07 16:00:00', 'Visiting the Colloseum and the Ancient Market'),
+(5, '2023-05-13 15:30:00', '2023-05-13 17:30:00', 'Exploring the canals of the city.'),
+(6, '2023-08-15 12:30:00', '2023-08-15 13:30:00', 'Visiting the Acropolis and The Acropolis Museum'),
+(7, '2023-09-13 17:30:00', '2023-09-13 19:30:00', 'Visiting the old part of Kalamata and watching a play at the Castle of Isabeau.'),
+(8, '2023-12-16 12:30:00', '2023-12-16 15:30:00', 'Visiting the White Tower Of Thessaloniki.'),
+(9, '2023-01-28 12:30:00', '2023-01-28 14:30:00', 'Eating at a local Restaurant.'),
+(10, '2023-02-23 16:30:00', '2023-02-23 18:00:00', 'Visiting the Barcelona FC Stadium.'),
+(11, '2023-05-24 15:00:00', '2023-05-24 17:30:00', 'Visiting a Spectacular small town close to Amsterdam, Delft.'),
+(12, '2023-07-10 16:30:00', '2023-07-10 18:30:00', 'Visiting the Big Ben'),
+(13, '2023-03-03 12:00:00', '2023-03-03 14:30:00', 'Visiting the old part of Kalamata and watching a play at the Castle of Isabeau.'),
+(14, '2023-04-02 11:30:00', '2023-04-02 13:00:00', 'Doing a tour of the villages of the island of Crete!'),
+(15, '2023-07-24 15:30:00', '2023-07-24 19:30:00', 'Swimming in the Local Beaches of the Island.'),
+(16, '2023-09-11 13:30:00', '2023-09-11 15:30:00', 'Visiting the Berlin Television Tower which is the tallest building in Berlin.'),
+(17, '2023-09-16 15:30:00', '2023-09-16 18:30:00', 'Taking a tour around this amazing island.'),
+(18, '2023-12-13 16:30:00', '2023-12-13 18:00:00', 'Learning about the prosperous history of Kalambaka.'),
+(19, '2023-10-08 12:30:00', '2023-10-08 15:00:00', 'Eating at a local Restaurant.'),
+(20, '2023-12-08 12:30:00', '2023-12-15 15:00:00', 'Visiting The Louvre Museum.');
 
 CREATE TABLE destination(
     dst_id INT(11) NOT NULL AUTO_INCREMENT,
@@ -266,16 +306,21 @@ CREATE TABLE destination(
 );
 
 INSERT INTO destination VALUES
-(null, 'Patra', 'It is a trully beautiful destination with a University campus', 'Local', 'Greek', 1),
-(null, 'Athena', 'The Capital city of Greece. It has a breathtaking view.', 'Local', 'Greek', 2),
-(null, 'Kerkyra', 'Greek Island with a Breathtaking view.', 'Local', 'Greek', 3),
-(null, 'Thessaloniki', 'Second biggest city in Greece. The subway just opened.', 'Local', 'Greek', 4),
-(null, 'Kalamata', 'Majestic city in the South of Greece with a lot of beaches.', 'Local', 'Greek', 5),
-(null, 'Santorini', 'Amzing Greek Island. Part of Kyklades', 'Local', 'Greek', 6),
-(null, 'Mykonos', 'The most famous Greek Island. It has a wild night life', 'Local', 'Greek', 7),
-(null, 'Paris', 'The most Romantic place anyone could visit', 'Abroad', 'French', 8),
-(null, 'Rome', 'Famous sites: Colloseum, Fontana di Trevi, Pantheon', 'Abroad', 'Italian', 9),
-(null, 'New York', 'The city who never sleeps.', 'Abroad', 'English', 10);
+(null, 'Kalamata', 'Great place for fans of theatre and the arts to visit. The main draw here is the Castle of Isabeau.', 'LOCAL', 'Greek', 1),
+(null, 'Nafpaktos', 'A stunning little port town across the Rio-Antirrio Bridge from Patras. Great place for a weekend trip away from Athens.', 'LOCAL', 'Greek', 2),
+(null, 'Corfu', 'It boasts some of the most beautiful beaches in all of Europe.', 'LOCAL', 'Greek', 3),
+(null, 'Kalambaka', ' This town is located in Meteora, giving it a stunning landscape as a backdrop to this picturesque city.', 'LOCAL', 'Greek', 4),
+(null, 'Heraklion', 'Heraklion, capital of the largest Greek islands, is a great place for those interested in Ancient Greek history.', 'LOCAL', 'Greek', 5),
+(null, 'Thessaloniki', 'Thessaloniki, known as the cultural capital of Greece, is also the second-largest city in Greece.', 'LOCAL', 'Greek', 6),
+(null, 'Rhodes', 'For those interested in Medieval history. This city is also a UNESCO World Heritage Site.', 'LOCAL', 'Greek', 7),
+(null, 'Athens', 'Lots of sightseeing here, especially for those interested in Ancient Greek history', 'LOCAL', 'Greek', 8),
+(null, 'Venice', 'Save this one for your one true love, it’s a magic spot to share with that special someone.', 'ABROAD', 'Italian', 9),
+(null, 'Amsterdam', 'This city is not only one of the most charming cities due to its character and being positioned on the canals, but it is also incredibly fun.', 'ABROAD', 'Dutch', 10),
+(null, 'Barcelona', 'It is a hub of new trends in the world of culture, fashion and cuisine.', 'ABROAD', 'Spanish', 11),
+(null, 'Rome', 'There’s no place like Rome and of course when in Rome, do as the Romans do.', 'ABROAD', 'Italian', 12),
+(null, 'Berlin', 'Germany’s capital has more history than you can comprehend even after spending weeks in the city.', 'ABROAD', 'German', 13),
+(null, 'London', 'There’s no other city quite like London and it should go without saying that you simply must visit once in your lifetime.', 'ABROAD', 'English', 14);
+(null, 'Paris', 'Paris... a girl’s dream. The croissants, crepes, baguettes… yes, this is Paris. You will be in love.', 'ABROAD', 'French', 15);
 
 CREATE TABLE travel_to(
     to_tr_id INT(11) NOT NULL,
@@ -290,16 +335,7 @@ CREATE TABLE travel_to(
 );
 
 INSERT INTO travel_to VALUES
-(1, 1, '1993-12-29 15:30:00', '1993-12-30 15:30:00'),
-(2, 2, '1994-12-29 15:30:00', '1994-12-30 15:30:00'),
-(3, 3, '1995-12-29 15:30:00', '1995-12-30 15:30:00'),
-(4, 4, '1996-12-29 15:30:00', '1996-12-30 15:30:00'),
-(5, 5, '1997-12-29 15:30:00', '1997-12-30 15:30:00'),
-(6, 6, '1998-12-29 15:30:00', '1998-12-30 15:30:00'),
-(7, 7, '1999-12-29 15:30:00', '1999-12-30 15:30:00'),
-(8, 8, '1992-11-29 15:30:00', '1992-11-30 15:30:00'),
-(9, 9, '1992-12-29 15:30:00', '1992-12-30 15:30:00'),
-(10, 10, '1992-12-29 15:30:00', '1992-12-30 15:30:00');
+
 
 CREATE TABLE reservation(
     res_tr_id INT(11) NOT NULL,
